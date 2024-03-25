0a8e00e33e7afaba4455e01d736e90c58a5cc28a
Manoj Aithani <aithanimanoj007@gmail.com>
2024-03-25 10:31:40 +0530

    initial commit 2:adding source code

---
+++ src/main/java/com/manoj/portfolio/webResume/WebResumeApplication.java
@@ -0,0 +1,13 @@
+package com.manoj.portfolio.webResume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebResumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebResumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manoj/portfolio/webResume/WebResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoj.portfolio.webResume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebResumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
